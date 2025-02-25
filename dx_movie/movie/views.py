3aa1838636b6d5de52c1bbd5f46cd01e878756ad
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-25 15:18:16 +0800

    添加settings.py中的apps下的rest_framework

--- dx_movie/movie/views.py
+++ dx_movie/movie/views.py
@@ -10,12 +10,13 @@ from django_filters.rest_framework import DjangoFilterBackend
 from utils.pagination import CustomPagination
 # 导入自定义的过滤器，用于对电影数据进行过滤。
 from .movie_filters import MovieFilter
-#
+# 导入视图集类，提供标准的 CRUD 操作接口。
 from rest_framework import viewsets
+# 导入 Django-filter 的模块，用于过滤器类的定义。
 from django_filters import rest_framework as filters
-
+# 导入权限类，用于限制权限。
 from rest_framework.permissions import IsAdminUser
-
+# 导入 Response 类，用于返回响应。
 from rest_framework.response import Response
 
 # class MovieList(generics.ListCreateAPIView):
@@ -34,7 +35,7 @@ from rest_framework.response import Response
 # class Category(generics.ListAPIView):
 #     queryset = Category.objects.all()
 #     serializer_class = CategorySerializer
-
+# 
 from rest_framework import permissions
 
 class IsAdminUserOrReadOnly(permissions.BasePermission):
@@ -49,23 +50,26 @@ class IsAdminUserOrReadOnly(permissions.BasePermission):
 
         # 仅管理员可进行其他操作
         return request.user.is_superuser
-
+# MovieFilter 类使用 Django-filter 来定义过滤条件，可以对电影的名称、类别 ID 和地区进行过滤：
 class MovieFilter(filters.FilterSet):
+    # 允许用户基于电影名称模糊搜索（忽略大小写）。
     movie_name = filters.CharFilter(lookup_expr='icontains')
+    # 允许基于分类 ID 进行精确过滤。
     category_id = filters.NumberFilter()
+    # 允许基于地区 ID 进行精确过滤。
     region = filters.NumberFilter()
 
     class Meta:
         model = Movie
         fields = ['movie_name', 'category_id', 'region']
-
+# MovieViewSet 是一个 ModelViewSet 类，提供了对 Movie 模型的 CRUD 操作（增删改查）接口：
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = MovieFilter
     permission_classes = [IsAdminUserOrReadOnly]
-    
+ # CategoryViewSet 也是一个 ModelViewSet 类，提供了对 Category 模型的 CRUD 操作接口：   
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
