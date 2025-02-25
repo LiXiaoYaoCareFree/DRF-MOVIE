c2cf74f43fb33b39571ac972ba8d7cd7a52b94e8
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-25 16:53:53 +0800

    创建项目首页

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="container" class="text-white text-sm bg-primary-300 min-h-screen pb-4"> 
+    <Header/>
+    <MovieList/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+import MovieList from '@/components/MovieList.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    Header, Footer, MovieList
+  },
 }
 </script>
